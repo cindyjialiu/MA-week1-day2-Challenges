1fcef48ce28636b77de6e41c7abfb81fe4f35d2b
Cindy <jl7e12@gmail.com>
2018-02-23 12:58:52 +0000

    wrapped dock_empty? and full? in private

--- Boris_bikes/lib/docking_station.rb
+++ Boris_bikes/lib/docking_station.rb
@@ -5,17 +5,15 @@ class DockingStation
     @arr = []
   end
 
-  def dock_empty?
-    @arr.length == 0
-  end
 
-  def release_bike#release_bike method separates dock_empty? and release_bike1 methods
-  #so we can test each method indepandently
-    release_bike1(dock_empty?)
-  end
 
-  def release_bike1(dock_empty)
-    if dock_empty
+  # def release_bike#release_bike method separates dock_empty? and release_bike1 methods
+  # #so we can test each method indepandently
+  #   release_bike1(dock_empty?)
+  # end
+
+  def release_bike
+    if dock_empty?
       raise "Sorry, no bike in the dock"
     else
       puts "Yes, you can get a bike"
@@ -23,19 +21,25 @@ class DockingStation
     end
   end
 
+ def dock(bike)
+   fail "Sorry, the capacity of the station was already filled" if full?
+   @arr.push(bike)
+   puts "This bike is now in the station"
+ end
+
+private
+  def dock_empty?
+    @arr.length == 0
+  end
+
+private
   def full?
     @arr.length >= 20
   end
 
- def dock1
-   dock(full?)
- end
-
- def dock(full)#separates full? and dock
-   fail "Sorry, the capacity of the station was already filled" if full
-   @arr.push(bike)
-   puts "This bike is now in the station"
- end
+ # def dock1
+ #   dock(full?)
+ # end
 
 end
 
--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -4,49 +4,49 @@ describe DockingStation do
   bike = Bike.new
 
 #type checking
-  it { should respond_to(:release_bike, :dock_empty?) }
+  #it { should respond_to(:release_bike, :dock_empty?) }
 
 #initialize test
-  it { expect(subject.arr).to eq []}
+  #it { expect(subject.arr).to eq []}
 
 #dock_empty? test when it's a empty array
-  it {
-    subject.arr = []
-    expect(subject.dock_empty?).to be true
-    expect(subject.arr).to eq []
-  }
-
-#dock_empty? test when it's not a empty array
-  it {
-    subject.arr = [1]
-    expect(subject.dock_empty?).to be false
-    expect(subject.arr).to eq [1]
-  }
+#   it {
+#     subject.arr = []
+#     expect(subject.dock_empty?).to be true
+#     expect(subject.arr).to eq []
+#   }
+#
+# #dock_empty? test when it's not a empty array
+#   it {
+#     subject.arr = [1]
+#     expect(subject.dock_empty?).to be false
+#     expect(subject.arr).to eq [1]
+#   }
 
 #release_bike test
   it {
     subject.arr = []
-    expect{subject.release_bike1(true)}.to raise_error("Sorry, no bike in the dock")
+    expect{subject.release_bike}.to raise_error("Sorry, no bike in the dock")
     expect(subject.arr).to eq []
   }
 
 #full? test when there are 20 bikes
-  it {
-    subject.arr = (1..20).map { |i| Bike.new }
-    expect(subject.full?).to be true
-    expect(subject.arr).to all( be_instance_of (Bike) )
-    expect(subject.arr.length).to be 20
-  }
+  # it {
+  #   subject.arr = (1..20).map { |i| Bike.new }
+  #   expect(subject.full?).to be true
+  #   expect(subject.arr).to all( be_instance_of (Bike) )
+  #   expect(subject.arr.length).to be 20
+  # }
 
 #dock test
   it {
     subject.arr = (1..20).map { |i| Bike.new }
-    expect{subject.dock(true)}.to raise_error("Sorry, the capacity of the station was already filled")
+    expect{subject.dock(Bike.new)}.to raise_error("Sorry, the capacity of the station was already filled")
     expect(subject.arr).to all( be_instance_of (Bike) )
     expect(subject.arr.length).to be 20
   }
 #type checking
-  # it { should respond_to(:dock).with(1).argument }
-  # it { expect(subject.dock(bike)).to match [bike] }
-  #it { expect { subject.bike_available? }.to raise_error("Sorry, no bike in the dock") }
+   #it { should respond_to(:dock).with(1).argument }
+
+
 end
